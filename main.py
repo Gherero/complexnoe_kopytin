372c12ec6ae9d1a2e96fb62a241cd269b1845e6a
gherero <katerina18>
2015-05-13 13:31:02 +0300

    Комплексное

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 
 
-A = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' * 2  # алфавит
+A = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' * 2  # алфавит
 def f(mc, k, op):
     k *= len(mc) // len(k) + 1
     return ''.join([A[A.index(j) + int(k[i]) * op] for i, j in enumerate(mc)])
@@ -13,23 +13,44 @@ def f(mc, k, op):
 def encrypt(message, key):      #шифрование текста
     return f(message, key, 1)
 
-def decrypt(ciphertext, key):   #дешифровка
+def decrypt(ciphertext, key):       #дешифровка
     return f(ciphertext, key, -1)
 
-def regist(username, passwrd):  #регистрация пользователей
+def regist(username, passwrd):      #регистрация пользователей
 
-    if(not len(passwrd)):
+    if(not len(passwrd)):           # если длина пароля 0
+        return -3
+
+    if(not len(username)):          # если длина логина 0
         return -2
 
-    if(r.exists(username)):
+    if(r.exists(username)):         #если пользователь существует
         return -1
 
-    hash_object = hashlib.sha256(passwrd.encode())
+    hash_object = hashlib.sha256(passwrd.encode()) # генерируем хеш
     hex_dig = hash_object.hexdigest()
-    r.set(username,hex_dig)
-    return 0
+    r.set(username,hex_dig)                         #заносим в БД имя пользователя (ключ) и хеш пароля (значение)
+    return 0                                        #сообщаем об успехе
 
-print(regist("kirill",""))
-print(encrypt('GRONSFELD', '2015'))  # шифрование
+def auth(username, passwrd):
 
-print(decrypt('IRPSUFFQF', '2015'))  # расшифровывание
+    if( not(len(username) and len(passwrd))):       #проверка длины логина , пароля
+        return -1
+
+    if(not r.exists(username)):                     #существует ли пользователь
+        return -2
+
+    hash_object = hashlib.sha256(passwrd.encode())  #генирируем хеш введенного пароля
+    hex_dig = hash_object.hexdigest()
+
+    dbhash=r.get(username)                          #достаем хеш из БД
+
+    if(dbhash==hex_dig):                            #сравниваем хеш из БД с введенным
+        return 0
+    print("Успех")
+
+
+print(auth("kirill","1234"))
+print(encrypt('kirill45', '2015'))  # шифрование
+
+print(decrypt('misnnl', '2015'))  # расшифровывание
